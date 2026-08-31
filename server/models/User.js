5254b0b4fa38c39be48f86fd5d01cef138696058
BETHEL <ifeanyichukwubethel2@gmail.com>
2026-08-30 22:44:12 -0300

    Fix User model pre-save hook and verify Forgot Password flow end-to-end

--- server/models/User.js
+++ server/models/User.js
@@ -82,9 +82,9 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
 };
 
 // Middleware to hash password before saving
-userSchema.pre('save', async function (next) {
+userSchema.pre('save', async function () {
     if (!this.isModified('password')) {
-        next();
+        return;
     }
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
